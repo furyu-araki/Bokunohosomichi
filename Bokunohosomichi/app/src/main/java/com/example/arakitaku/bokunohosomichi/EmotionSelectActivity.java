87b72f9dcc0656b1d07624f66abd87996d8a97cd
umama0716 <rustydeer@gmail.com>
2016-04-15 13:20:56 +0900

    喜怒哀楽ボタンを追加

--- Bokunohosomichi/app/src/main/java/com/example/arakitaku/bokunohosomichi/EmotionSelectActivity.java
+++ Bokunohosomichi/app/src/main/java/com/example/arakitaku/bokunohosomichi/EmotionSelectActivity.java
@@ -3,6 +3,8 @@ package com.example.arakitaku.bokunohosomichi;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * 画像選択画面
@@ -13,5 +15,9 @@ public class EmotionSelectActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emotion_select);
+    }
+
+    public void b0(View v) {
+        ((TextView) findViewById(R.id.top_text)).setText("ほげ");
     }
 }

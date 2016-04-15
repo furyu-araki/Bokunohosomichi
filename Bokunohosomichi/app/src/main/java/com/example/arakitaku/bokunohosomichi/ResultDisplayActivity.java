72c63ba5b3fcfc25d6bb78f4be7a88ce6d2ab2c9
furyu-araki <araki.taku@furyu.jp>
2016-04-15 14:59:40 +0900

    結合完了！！

--- Bokunohosomichi/app/src/main/java/com/example/arakitaku/bokunohosomichi/ResultDisplayActivity.java
+++ Bokunohosomichi/app/src/main/java/com/example/arakitaku/bokunohosomichi/ResultDisplayActivity.java
@@ -3,6 +3,7 @@ package com.example.arakitaku.bokunohosomichi;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 /**
  * 画像選択画面
@@ -13,6 +14,9 @@ public class ResultDisplayActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_display);
+
+        ImageView resultImage = (ImageView) findViewById(R.id.result_display_imageView);
+        resultImage.setImageURI(ImageSelectResult.getInstance().GetFileUrl());
 
         VerticalTextLinearLayout verticalText1 = (VerticalTextLinearLayout) findViewById(R.id.result_display_vertical_text1);
         verticalText1.setText("あいうえお");

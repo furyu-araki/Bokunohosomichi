c9875b270cb329a20ecff5b4ba46a71c26fac5ea
furyu-araki <araki.taku@furyu.jp>
2016-04-15 13:24:41 +0900

    タイトル画面から画像選択画面への遷移を記述した

--- Bokunohosomichi/app/src/main/java/com/example/arakitaku/bokunohosomichi/TitleActivity.java
+++ Bokunohosomichi/app/src/main/java/com/example/arakitaku/bokunohosomichi/TitleActivity.java
@@ -1,7 +1,9 @@
 package com.example.arakitaku.bokunohosomichi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+
+        // スタートボタン
+        findViewById(R.id.title_button_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TitleActivity.this, ImageSelectActivity.class));
+            }
+        });
     }
 }
